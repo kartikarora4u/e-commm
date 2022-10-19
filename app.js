31eb5c8f5f97942c215b17c19751fcdf92d9223b
Kartik Arora <kartikarora7774@gmail.com>
2022-10-19 21:38:50 +0530

    update colors

--- app.js
+++ app.js
@@ -98,7 +98,6 @@ app.post("/login", (request, response) => {
           response.status(200).send({
             message: "Login Successful",
             email: user.email,
-            billing_id: user.billing_id,
             token,
           });
         })
